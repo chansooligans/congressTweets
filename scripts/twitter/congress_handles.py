6ae4ae5d4ddf866119e62920e32c2f4fe7c689e2
chansooligans <cs2737@nyu.edu>
2023-05-23 17:54:07 -0400

    script to download handles

---
+++ scripts/twitter/congress_handles.py
@@ -0,0 +1,19 @@
+# %%
+from dataclasses import dataclass
+from ideoquant.twitter.handles import CongressHandles
+
+# %%
+senators = CongressHandles(
+    url="https://ucsd.libguides.com/congress_twitter/senators", _type="Senator"
+)
+senate_handles = senators.get_handles()
+senate_handles.to_csv("../../data/senator_handles.csv")
+
+# %%
+reps = CongressHandles(
+    url="https://ucsd.libguides.com/congress_twitter/reps", _type="Representative"
+)
+rep_handles = reps.get_handles()
+senate_handles.to_csv("../../data/rep_handles.csv")
+
+# %%
