6b087e55ab22dbf55cee5efa47e1555ffa4268d2
chansooligans <cs2737@nyu.edu>
2023-05-31 09:28:03 -0400

    LDA model

--- scripts/explore/nlp.py
+++ scripts/explore/nlp.py
@@ -1,5 +1,6 @@
 # %%
 import pandas as pd
+import re
 from gensim import corpora, models
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -18,6 +19,7 @@ stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
 def preprocess_text(text):
+    text = re.sub(r'\b(?:https?://|www\.)\S+\b', '', text)
     tokens = word_tokenize(text.lower())  # Tokenize the text and convert to lowercase
     tokens = [token for token in tokens if token.isalpha()]  # Remove non-alphabetic tokens
     tokens = [token for token in tokens if token not in stop_words]  # Remove stopwords
@@ -34,9 +36,36 @@ dictionary = corpora.Dictionary(df['preprocessed_text'])
 corpus = [dictionary.doc2bow(doc) for doc in df['preprocessed_text']]
 
 # %%
+%%time
 # Perform topic modeling using LDA
 lda_model = models.LdaModel(corpus, num_topics=10, id2word=dictionary, passes=10)
 
+# %%
+%%time
+document_topics = [lda_model.get_document_topics(doc) for doc in corpus]
+
+# %%
+# Calculate the marginal topic distribution
+marginal_topic_distribution = [0] * lda_model.num_topics
+for topics in document_topics:
+    for topic_num, prob in topics:
+        marginal_topic_distribution[topic_num] += prob
+
+# Normalize the marginal topic distribution
+marginal_topic_distribution /= sum(marginal_topic_distribution)
+marginal_topic_distribution = [
+    (prob, topic_num) 
+    for topic_num, prob in enumerate(marginal_topic_distribution)
+]
+marginal_topic_distribution = sorted(marginal_topic_distribution, key=lambda x: -x[0])
+
+# %%
 # Print the topics and their associated keywords
-for topic_num, words in lda_model.print_topics():
-    print(f"Topic #{topic_num}: {words}")
+topics = lda_model.print_topics()
+
+for prob, topic_num in marginal_topic_distribution:
+    print(f"Topic #{topic_num}: Probability={prob}")
+    print(f"\tWords: {topics[topic_num][1]}")
+
+# %%
+# %%
